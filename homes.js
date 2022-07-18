bb66754fb5f3375aa9722f11ae1005531460904b
BrendonW250 <brendonwright16@gmail.com>
2022-07-18 18:46:34 -0400

    just beginning

---
+++ homes.js
@@ -0,0 +1,23 @@
+// created API with all nursing homes 
+// who aide dementia patients in the Bronx
+
+let nursingHomes = {
+   'Bainbridge Nursing & Rehabilitation Center': {
+       imageUrl: '',
+       name: 'Bainbridge Nursing & Rehabilitation Center',
+       address: 'Somewhere on Gunhill Rd'
+   },
+
+   'Morningside Nursing & Rehabilitation': {
+       imageUrl: '',
+       name: 'Morningside Nursing & Rehabilitation',
+       address: '1000 Pelham Parkway S, Bronx, NY'
+   }, 
+
+   'Beth Abraham Housing Weinberg Gardens': {
+       imageUrl: '',
+       name: 'Beth Abraham Housing Weinberg Gardens',
+       address: '2260 Barker Ave. Bronx, NY'
+   }
+
+}
