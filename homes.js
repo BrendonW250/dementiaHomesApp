2802cafad86116d0af7ddb7a91d39dd5a3c7e077
BrendonW250 <brendonwright16@gmail.com>
2022-07-23 19:07:17 -0400

    progress

--- homes.js
+++ homes.js
@@ -18,6 +18,48 @@ let nursingHomes = {
        imageUrl: '',
        name: 'Beth Abraham Housing Weinberg Gardens',
        address: '2260 Barker Ave. Bronx, NY'
+   },
+
+   'Bronx Center For Rehab Health': {
+       imageUrl: '',
+       name: 'Bronx Center For Rehab Health',
+       address: '1010 Underhill Ave., Bronx, NY, 10472'
+   },
+
+   'St. Patricks Home Rehabilitation': {
+       imageUrl: '',
+       name: 'St. Patricks Home Rehabilitation',
+       address: '66 Van Cortlandt Park South, Bronx, NY, 10463'
+   },
+
+   'Grand Manor Nursing and Rehab': {
+       imageUrl: '',
+       name: 'Grand Manor Nursing and Rehab',
+       address: '700 White Plains Road, Bronx, NY, 10473'
+   },
+
+   'University Nursing Home': {
+       imageUrl: '',
+       name: 'University Nursing Home' ,
+       address: '2505 Grand Ave, Bronx, NY, 10468'
+   },
+
+   'Kings Harbor Multicare Center': {
+       imageUrl: '',
+       name: 'Kings Harbor Multicare Center',
+       address: '2000 E Gunhill Road, Bronx, NY, 10469'
+   },
+
+   'Eastchester Rehab and Health Care Center': {
+       imageUrl: '',
+       name: 'Eastchester Rehab and Health Care Center',
+       address: '2700 Eastchester Road, Bronx, NY, 10469'
+   },
+
+   'Williamsbridge Manor Nursing Home': {
+       imageUrl: '',
+       name: 'Williamsbridge Manor Nursing Home',
+       address: '1540 Tomlinson Ave, Bronx, NY, 10461'
    }
 
 }
--- server.js
+++ server.js
@@ -1,1 +1,31 @@
 const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+
+
+
+// Middleware
+app.use(bodyParser.urlencoded({
+    extended: true
+}))
+// Allows for the server to read front-end code (the css)
+app.use(express.static('public'))
+
+// routes
+app.get('/', (request, response) => {
+    response.sendFile(__dirname + '/index.html')
+    
+})
+
+app.get('/bainbridge', (request, response) => {
+    response.sendFile(__dirname + '/routesToDiffHomes/bainbridge.html')
+})
+
+// .catch(error => console.error(error))
+
+app.listen(3000, function() {
+    console.log('listening on 3000')
+})
+// app.listen(process.env.PORT || PORT, () => {
+//     console.log('Server is running.')
+// })
