5eeebf4df1a7276cc448006bdd4f1fc55bab4f6f
BrendonW250 <brendonwright16@gmail.com>
2022-07-31 16:59:15 -0400

    testing-query-selectors

---
+++ public/assets/infoForHomes/info.js
@@ -0,0 +1,25 @@
+
+// Client side js and how the info will
+// be displayed when each home is clicked
+
+// listens for a 'click' on any <a> in the index.html file 
+// Once clicked the getFetch file will trigger
+document.querySelector('a').addEventListener('click', getFetch)
+
+async function getFetch(){
+    const homeName = document.querySelector('a').value 
+
+    try{
+
+        const response = await fetch(`https://bronx-dementia-homes.herokuapp.com/${homeName}`)
+        const data = await response.json()
+
+        console.log(data)
+
+        // displaying the name of the home from the api
+        document.querySelector('h2').innerHTML = 'Name ' + data.name
+    }catch(error){
+        console.log(error)
+    }
+    
+}
